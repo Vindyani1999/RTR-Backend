5091b6aff6d88fedfced60c0c362a8e64382739f
Vindyani1999 <k.a.c.h.vindyani@gmail.com>
2024-10-28 18:48:59 +0530

    view all bookings

--- src/models/Booking.ts
+++ src/models/Booking.ts
@@ -1,17 +1,10 @@
 import mongoose, { Schema, Document } from "mongoose";
 
-interface CartItem {
-  name: string;
-  price: number;
-  quantity: number;
-}
-
-interface Booking extends Document {
+export interface BookingDocument extends Document {
   tableNumber: number;
-  tableType: string;
+  tableType: string; // Corresponds to the Table type
   tablePrice: number;
   numberOfChairs: number;
-  stableStatus: string;
   date: string;
   startTime: string;
   endTime: string;
@@ -20,8 +13,6 @@ interface Booking extends Document {
   phoneNumber: string;
   numberOfPeople: number;
   address: string;
-  cartItems: CartItem[];
-  isConfirmed: boolean;
 }
 
 const BookingSchema: Schema = new Schema({
@@ -29,7 +20,6 @@ const BookingSchema: Schema = new Schema({
   tableType: { type: String, required: true },
   tablePrice: { type: Number, required: true },
   numberOfChairs: { type: Number, required: true },
-  stableStatus: { type: String, default: "Available" },
   date: { type: String, required: true },
   startTime: { type: String, required: true },
   endTime: { type: String, required: true },
@@ -38,14 +28,6 @@ const BookingSchema: Schema = new Schema({
   phoneNumber: { type: String, required: true },
   numberOfPeople: { type: Number, required: true },
   address: { type: String, required: true },
-  cartItems: [
-    {
-      name: { type: String, required: true },
-      price: { type: Number, required: true },
-      quantity: { type: Number, required: true },
-    },
-  ],
-  isConfirmed: { type: Boolean, default: false },
 });
 
-export default mongoose.model<Booking>("Booking", BookingSchema);
+export default mongoose.model<BookingDocument>("Booking", BookingSchema);
